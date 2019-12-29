a882b0439f9f78b88ef3b19f1e016f74e469ef34
Marc Enzo <emarc237@gmail.com>
2019-12-29 03:55:11 +0100

    feat(auth): authentication middleware
    Authenticate every requests made on shop via the auth middleware.

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Role;
+use App\User;
+use Closure;
+use Illuminate\Support\Facades\DB;
+
+class AuthMiddleware
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param $request
+	 * @param Closure $next
+	 * @return \Illuminate\Http\JsonResponse|mixed
+	 * @throws \Exception
+	 */
+	public function handle($request, Closure $next)
+	{
+		if (!$request->hasHeader('Authorization'))
+			return response()->json('Authorization Header not found.', 401);
+
+		$token = $request->header('Authorization');
+
+		if ($token == null)
+			return response()->json('No token provided.', 401);
+
+		try {
+			$user = User::all()->where('token', $token)->first();
+			
+			if ($user != null) {
+				$method = $request->method();
+				$url_array = explode('/', $request->getRequestUri());
+				$shop_position = array_search('shop', $url_array);
+				
+				if ($shop_position != false) {
+					$shop = $url_array[$shop_position + 1];
+					$page = $url_array[$shop_position + 2];
+					
+					// Check if user can access shop.
+					$can_access = DB::table('users_shops')->where('user', $user->id)->where('shop', $shop)->first();
+					
+					if ($can_access != null) {
+						$role_id = DB::table('users_roles')->where('user', $user->id)->where('shop', $shop)->first();
+						
+						// Check if user have authorization.
+						if ($role_id != null) {
+							$role_id = $role_id->role;
+							$role = Role::find($role_id);
+							
+							if ($role != null) {
+								$actions = $role->actions;
+								$can = false;
+								
+								if ($method == 'GET')
+									$to_check = 'view_' . $page;
+								else
+									$to_check = 'manage_' . $page;
+								
+								foreach ($actions as $action) {
+									if ($action == $to_check)
+										$can = true;
+								}
+								
+								if ($can)
+									return $next($request);
+								else
+									return response()->json(['message' => "You don't have permission to access this section."], 401);
+							} else {
+								return response()->json(['message' => "You don't have permission to access this section."], 401);
+							}
+						} else {
+							return response()->json(['message' => "You don't have permission to access this section."], 401);
+						}
+					} else {
+						return response()->json(['message' => "You don't have permission to manage this shop."], 401);
+					}
+				}
+			} else {
+				return response()->json(['message' => "Invalid token provided."], 401);
+			}
+		} catch (\Exception $e) {
+			return response()->json(['message' => $e->getMessage()], 401);
+		}
+	}
+}
