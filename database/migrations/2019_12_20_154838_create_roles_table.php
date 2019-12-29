eb42fdb9da5fbeca8649468003f30321b6be6949
Marc Enzo <emarc237@gmail.com>
2019-12-29 03:54:11 +0100

    feat(role): user role by shop
    Creating table to define user role by shop.

---
+++ database/migrations/2019_12_20_154838_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('shop')->nullable();
+            $table->string('name');
+            $table->json('actions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
